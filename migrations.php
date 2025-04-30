a5ec59bf896f4f42347f22b1c0b8e6aa5b6aae9d
Maurizio Giambrone <mgiambrone.dev@gmail.com>
2025-04-30 10:20:53 +0000

    Added userClass to config array

--- migrations.php
+++ migrations.php
@@ -1,5 +1,6 @@
 <?php
 use gdev\phpmvc\Application;
+use app\models\User;
 
 require_once 'vendor/autoload.php';
 
@@ -7,6 +8,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $config = [
+    'userClass' => User::class,
     'db' => [
         'dsn' => $_ENV['DB_DSN'],
         'user' => $_ENV['DB_USERNAME'],
